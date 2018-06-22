b2445f80357885a495530c9304f746324c3da9dd
Megha Ruparelia <rupareliamegha@gmail.com>
2018-06-21 21:39:59 -0500

    Update InrhythmChallengeTest.java

--- src/test/java/com/inrhythm/challenge/InrhythmChallengeTest.java
+++ src/test/java/com/inrhythm/challenge/InrhythmChallengeTest.java
@@ -8,61 +8,53 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
 
-
 public class InrhythmChallengeTest {
-	
+
 	@Test
-	public void testBaseCase()
-	{
-	    MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("The cow jumped over the moon.");
-	    List<String> checkWords = t1.getWords();
-	    assertEquals(6,t1.getLength());
-	    assertThat(checkWords,contains("jumped"));
+	public void testBaseCase() {
+		MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("The cow jumped over the moon.");
+		List<String> checkWords = t1.getWords();
+		assertEquals(6, t1.getLength());
+		assertThat(checkWords, contains("jumped"));
 	}
-	
-	
+
 	@Test
-	public void testMultipleSameLength()
-	{
-	    MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("   The cow didn't jumped over the moon ");
-	    List<String> checkWords = t1.getWords();
-	    assertEquals(6,t1.getLength());
-	    assertThat(checkWords,contains("didn't","jumped"));
+	public void testMultipleSameLength() {
+		MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("   The cow didn't jumped over the moon ");
+		List<String> checkWords = t1.getWords();
+		assertEquals(6, t1.getLength());
+		assertThat(checkWords, contains("didn't", "jumped"));
 	}
-	
+
 	@Test
-	public void testEmptyString()
-	{
-	    MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("");
-	    List<String> checkWords = t1.getWords();
-	    assertEquals(0,t1.getLength());
-	    assertThat(checkWords,empty());
+	public void testEmptyString() {
+		MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("");
+		List<String> checkWords = t1.getWords();
+		assertEquals(0, t1.getLength());
+		assertThat(checkWords, empty());
 	}
-	
+
 	@Test
-	public void testSingleString()
-	{
-	    MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("cow");
-	    List<String> checkWords = t1.getWords();
-	    assertEquals(3,t1.getLength());
-	    assertThat(checkWords,contains("cow"));
+	public void testSingleString() {
+		MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("cow");
+		List<String> checkWords = t1.getWords();
+		assertEquals(3, t1.getLength());
+		assertThat(checkWords, contains("cow"));
 	}
-	
+
 	@Test
-	public void testSpecialCharString()
-	{
-	    MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("  ?? ");
-	    List<String> checkWords = t1.getWords();
-	    assertEquals(0,t1.getLength());
-	    assertThat(checkWords,empty());
+	public void testSpecialCharString() {
+		MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("  ?? ");
+		List<String> checkWords = t1.getWords();
+		assertEquals(0, t1.getLength());
+		assertThat(checkWords, empty());
 	}
-	
+
 	@Test
-	public void testMultiString()
-	{
-	    MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("What is your name? do you stay around?");
-	    List<String> checkWords = t1.getWords();
-	    assertEquals(6,t1.getLength());
-	    assertThat(checkWords,contains("around"));
+	public void testMultiString() {
+		MaxLengthWords t1 = InrhythmChallenge.getMaxLengthWords("What is your name? do you stay around?");
+		List<String> checkWords = t1.getWords();
+		assertEquals(6, t1.getLength());
+		assertThat(checkWords, contains("around"));
 	}
 }
