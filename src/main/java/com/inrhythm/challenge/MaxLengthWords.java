8fd7cbefe02730233f6646d835f2ea46e0085204
Megha Ruparelia <rupareliamegha@gmail.com>
2018-06-21 21:38:20 -0500

    Update MaxLengthWords.java
    Formatted code

--- src/main/java/com/inrhythm/challenge/MaxLengthWords.java
+++ src/main/java/com/inrhythm/challenge/MaxLengthWords.java
@@ -1,36 +1,26 @@
 package com.inrhythm.challenge;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class MaxLengthWords
-{
-    private final int length;
-    private final List<String> words;
+public class MaxLengthWords {
+	private final int length;
+	private final List<String> words;
 
-    public MaxLengthWords(final int length, final List<String> words)
-    {
-        this.length = length;
-        this.words = new ArrayList<>(words);
-    }
+	public MaxLengthWords(final int length, final List<String> words) {
+		this.length = length;
+		this.words = words;
+	}
 
-    public List<String> getWords()
-    {
-        return words;
-    }
+	public List<String> getWords() {
+		return words;
+	}
 
-    public int getLength()
-    {
-        return this.length;
-    }
+	public int getLength() {
+		return this.length;
+	}
 
-
-    @Override
-    public String toString()
-    {
-        return "MaxLengthWords{" +
-            "length=" + length +
-            ", words=" + words +
-            '}';
-    }
+	@Override
+	public String toString() {
+		return "MaxLengthWords{" + "length=" + length + ", words=" + words + '}';
+	}
 }
